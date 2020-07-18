9abae65e4220d6e3cc06a531fb26dfd81448f818
Preston Patterson <prestonjpatterson@gmail.com>
2020-07-18 01:32:00 -0600

    got the basic funcshoionalaty done.

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,44 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+const util = require("util")
+
+const writeToFileAsync = util.promisify(fs.writeFile)
 // array of questions for user
-const questions = [
 
-];
-
+const askQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "What do you want you title to be?"
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "What do you want you description to be?"
+        }
+    ])
+}
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(answers) {
+    return `
+    #${answers.Title}
+    ${answers.Description}`;
 }
 
 // function to initialize program
-function init() {
+askQuestions()
+    .then(function (answers) {
+        const readMe = writeToFile(answers);
 
-}
+        return writeToFileAsync("README.md", readMe)
+    })
+    .then(function () {
+        console.log("Your README is done.")
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 
 // function call to initialize program
-init();
+
